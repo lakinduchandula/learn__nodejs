58fd05722be58ce90539fba7d91df70991f02d62
lakinduchandula <lakindu.chandula@hotmail.com>
2021-02-24 11:58:30 +0530

    end of MVC module

--- app.js
+++ app.js
@@ -2,25 +2,23 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
+
+const errorController = require('./controllers/error');
 
 const app = express();
 
-app.engine('hbs', expressHbs());
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'page not found'});
-});
+app.use(errorController.get404);
 
 app.listen(3000);
