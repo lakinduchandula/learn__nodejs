ae3ff0fec658c14b517918fd119aec4c61967e02
lakinduchandula <lakindu.chandula@hotmail.com>
2021-03-03 18:40:25 +0530

    end of module 11 - SQL

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'academind__nodejs',
-    password: 'Lakindu@123'
+const sequelize = new Sequelize('academind__nodejs', 'root', 'Lakindu@123', {
+  dialect: 'mysql',
+  host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
