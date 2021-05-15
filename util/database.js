69ccc36e79cf5db176c65032523873978d6ba662
lakinduchandula <lakindu.chandula@hotmail.com>
2021-05-15 22:26:27 +0530

    clean repo

--- util/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('academind__nodejs', 'root', 'Lakindu@123', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
-
-module.exports = sequelize;
