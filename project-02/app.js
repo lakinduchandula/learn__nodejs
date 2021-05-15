7494b6eb916f42555d8f2404f808560fb75e7372
lakinduchandula <lakindu.chandula@hotmail.com>
2021-05-15 23:48:56 +0530

    node life cycle and event loop

--- project-02/app.js
+++ project-02/app.js
@@ -1,7 +1,11 @@
-const http = require('http');
+const http = require('http'); 
 
-const server = http.createServer((req, res) => {
-    console.log(req);
+const server = http.createServer((req, res) => { 
+    console.log(req.url, req.method, req.headers); // output the incoming req
+
+    // this will quite from registerd process and back to the command line 
+    // (we are don't do it in production because we dont need to down our server)
+    //process.exit(); 
 });
 
-server.listen(3000);
+server.listen(3000); // this line will listen through port 3000 when it's done it will run line 3
