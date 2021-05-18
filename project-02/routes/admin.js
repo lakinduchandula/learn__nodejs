4fa342b1b95918a53eb59ca59c4b8b0f36bbb1db
lakinduchandula <lakindu.chandula@hotmail.com>
2021-05-18 18:12:42 +0530

    Sharing Data Across Requests & Users

--- project-02/routes/admin.js
+++ project-02/routes/admin.js
@@ -1,19 +1,22 @@
 const path = require('path');
 const express = require("express");
 
-const router = express.Router();
+const routes = express.Router();
+
+const products = [] ;
 
 // we can put get, post method insted of use after app. ,
 // reach under /admin/add-product => GET
-router.get("/add-product", (req, res, next) => {
+routes.get("/add-product", (req, res, next) => {
     res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
 // app.post mean it will only run under post method
 // reach under /admin/add-product => POST
-router.post("/add-product", (req, res, next) => {
-  console.log(req.body);
+routes.post("/add-product", (req, res, next) => {
+  products.push({title: req.body.title});
   res.redirect("/");
 });
 
-module.exports = router;
+exports.routes = routes;
+exports.products = products;
--- project-02/routes/shop.js
+++ project-02/routes/shop.js
@@ -1,9 +1,13 @@
 const path = require('path');
 const express = require("express");
 
+// custom libraries
+const adminData = require('./admin');
+
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
+    console.log('shop.js' ,adminData.products)
     res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
