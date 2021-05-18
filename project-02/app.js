5b009bbd4f6ee59cb0e9eb72bb361376f0399986
lakinduchandula <lakindu.chandula@hotmail.com>
2021-05-18 13:02:19 +0530

    learn about get/post req in middleware func

--- project-02/app.js
+++ project-02/app.js
@@ -3,19 +3,21 @@ const express = require("express");
 
 const app = express(); // this express will handle almost very thing in behind the scenes
 
-app.use((req, res, next) => {
-  // use allow us to add another middleware function
-  /**
-   * This function will run for every incoming request
-   * next argument means that if we only call next();
-   * then it will only allow travel to other middleware function
-   * **/
-  console.log("This is middleware");
-  next();
+app.use(express.urlencoded({ // this will help to catch the body in express package
+  extended: true
+}));
+
+app.use('/add-product', (req, res, next) => {
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
-app.use((req, res, next) => {
-  console.log("This is another middleware");
+// we can put get, post method insted of use after app. , app.post mean it will only run under post method
+app.post('/product', (req, res, next) => { 
+  console.log(req.body);
+  res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
   res.send('<h1>Hello from express.js</h1>'); //  this send(); method can use to send a almost any response
 });
 
