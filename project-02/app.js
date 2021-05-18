eefee9b83d09c519f0642736337c2062e83b1a0d
lakinduchandula <lakindu.chandula@hotmail.com>
2021-05-18 08:38:23 +0530

    learn about middleware function and method use();

--- project-02/app.js
+++ project-02/app.js
@@ -1,9 +1,24 @@
-const http = require('http');
+const http = require("http");
 
 // 3rd party libraries
-const express = require('express');
+const express = require("express");
 
 const app = express(); // this express will handle almost very thing in behind the scenes
+
+app.use((req, res, next) => {
+  // use allow us to add another middleware function
+  /**
+   * This function will run for every incoming request
+   * next argument means that if we only call next();
+   * then it will only allow travel to other middleware function
+   * **/
+  console.log("This is middleware");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("This is another middleware");
+});
 
 const server = http.createServer(app); // app is valid request handler
 
