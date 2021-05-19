9ea8c59ec9043f2f140f93aaef22b2873f1a1634
lakinduchandula <lakindu.chandula@hotmail.com>
2021-05-19 16:36:09 +0530

    create the shop structure

--- project-02/controllers/products.js
+++ project-02/controllers/products.js
@@ -2,7 +2,7 @@ const Product = require("../models/product");
 
 // export this get method add product middleware func
 exports.getAddProduct = (req, res, next) => {
-  res.render("add-product", {
+  res.render("admin/add-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
     formsCSS: true,
@@ -22,7 +22,7 @@ exports.postAddProduct = (req, res, next) => {
 exports.getProducts = (req, res, next) => {
   // directly call to static func through class name Product
   Product.fetchAll(products => { 
-    res.render("shop", {
+    res.render("shop/product-list", {
       prods: products,
       pageTitle: "Shop",
       path: "/",
