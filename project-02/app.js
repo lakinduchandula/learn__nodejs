a228723de0e0316d6713c15092d80e21a0290dde
lakinduchandula <lakindu.chandula@hotmail.com>
2021-05-24 13:18:23 +0530

    user added (hardcode)

--- project-02/app.js
+++ project-02/app.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require("path");
 
 // 3rd party libraries
 const express = require("express");
@@ -6,34 +6,48 @@ const express = require("express");
 // custom (my own) libraries
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-const mongoConnect = require('./utils/database').mongoConnect;
+const mongoConnect = require("./utils/database").mongoConnect;
+const User = require("./models/user");
 
 // import controllers
-const pageNotFoundController = require('./controllers/404');
+const pageNotFoundController = require("./controllers/404");
 
 const app = express(); // this express will handle almost very thing in behind the scenes
 
-app.set('view engine', 'ejs'); // this will setup ejs as the template engine
+app.set("view engine", "ejs"); // this will setup ejs as the template engine
 
-// this will setup where temp file in, 
+// this will setup where temp file in,
 //if temp file are is some where besides views folder second argument should replace by that folder name
-app.set('views', 'views');  
+app.set("views", "views");
 
+app.use(
+  express.urlencoded({
+    // this will help to catch the body in express package
+    extended: true,
+  })
+);
 
-app.use(express.urlencoded({ // this will help to catch the body in express package
-  extended: true
-}));
+app.use((req, res, next) => {
+  User.findById("60ab59a2b3ccbe279141f972")
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  next();
+});
 
 // this middleware function will give the access to the user to read our file system in public folder
-app.use(express.static(path.join(__dirname, 'public'))); 
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin', adminRoutes); // handling all admin routes
+app.use("/admin", adminRoutes); // handling all admin routes
 app.use(shopRoutes); // handling all shop routes
 
 app.use(pageNotFoundController.NotFoundPage);
 
-mongoConnect(() => { // call the database fuction in util/database.js have
+mongoConnect(() => {
+  // call the database fuction in util/database.js have
   app.listen(3000); // this will do both creating server and listen on port 3000
-})
-
-
+});
---
+++ project-02/models/user.js
@@ -0,0 +1,40 @@
+const mongodb = require("mongodb");
+
+// 3rd party libraries
+const getDb = require("../utils/database").getDb;
+
+class User {
+  constructor(username, email) {
+    this.name = username;
+    this.email = email;
+  }
+
+  save() {
+    const db = getDb(); // get database attached to node appication
+    return db
+      .collection("users")
+      .insertOne(this)
+      .then(result => {
+        console.log(result);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  static findById(userId) {
+    const db = getDb(); // get database attached to node appication
+    return db
+      .collection("users")
+      .find({ _id: mongodb.ObjectID(userId) })
+      .next()
+      .then(user => {
+        console.log("User Found!");
+        return user;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+}
+ 
