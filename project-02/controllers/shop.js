533c65e71af0214472630a75e7acc4e60da31fae
lakinduchandula <lakindu.chandula@hotmail.com>
2021-06-02 20:13:21 +0530

    finished pagination logic, implemented. end mo 21

--- project-02/controllers/shop.js
+++ project-02/controllers/shop.js
@@ -9,7 +9,7 @@ const Product = require("../models/product");
 const Order = require("../models/order");
 
 //*** constants
-const ITEMS_PER_PAGE = 4;
+const ITEMS_PER_PAGE = 5;
 
 // export this get methods shop product middleware func
 exports.getProducts = (req, res, next) => {
@@ -18,13 +18,40 @@ exports.getProducts = (req, res, next) => {
    * find() method is inside of mongoose so it will give all product but it didn't  *
    * give the cursor back, but if we nedd .cursor() we can use .next() as well      *
    **********************************************************************************/
+  // Product.find()
+  //   .then(products => {
+  //     res.render("shop/product-list", {
+  //       prods: products,
+  //       pageTitle: "All Products",
+  //       path: "/products",
+  //       isAuthenticated: req.session.isLoggedIn,
+  //     });
+  //   })
+  // if "+req.query.page" is not holding value page is equal to 1
+  const page = +req.query.page || 1;
+  let totalItems;
+
   Product.find()
+    .countDocuments()
+    .then(numberProducts => {
+      totalItems = numberProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then(products => {
       res.render("shop/product-list", {
         prods: products,
         pageTitle: "All Products",
         path: "/products",
         isAuthenticated: req.session.isLoggedIn,
+        currentPage: page,
+        totalProducts: totalItems,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
       });
     })
     .catch(err => {
@@ -36,17 +63,31 @@ exports.getProducts = (req, res, next) => {
 
 // Index Page Controller
 exports.getIndex = (req, res, next) => {
-  const page = req.query.page;
+  // if "+req.query.page" is not holding value page is equal to 1
+  const page = +req.query.page || 1;
+  let totalItems;
 
   Product.find()
-    .skip((page - 1) * ITEMS_PER_PAGE)
-    .limit(ITEMS_PER_PAGE)
+    .countDocuments()
+    .then(numberProducts => {
+      totalItems = numberProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then(products => {
       res.render("shop/index", {
         prods: products,
         pageTitle: "Shop",
         path: "/",
         isAuthenticated: req.session.isLoggedIn,
+        currentPage: page,
+        totalProducts: totalItems,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
       });
     })
     .catch(err => {
