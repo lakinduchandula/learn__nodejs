0fb36c08e6b8a7b8890067d2675a3e16b8ab5c0e
lakinduchandula <lakindu.chandula@hotmail.com>
2021-06-08 16:08:43 +0530

    handling errors

--- project-03/app.js
+++ project-03/app.js
@@ -38,6 +38,15 @@ app.use(
     schema: graphqlSchema,
     rootValue: graphqlResolver,
     graphiql: true,
+    formatError(err) {
+      if (!err.originalError) {
+        return err;
+      }
+      const data = err.originalError.data;
+      const message = err.originalError.message || "An Error Occurred";
+      const code = err.originalError.code || 500;
+      return { message: message, status: code, data: data };
+    },
   })
 );
 
--- project-03/graphql/resolvers.js
+++ project-03/graphql/resolvers.js
@@ -1,22 +1,35 @@
 const User = require("../models/user");
 
 const bcrypt = require("bcryptjs");
+const validator = require("validator");
 
 module.exports = {
   createUser({ userInput }, req) {
-    // const email = args.userInput.email;
-    const email = userInput.email;
-    return User.findOne({ email: email })
+    const errors = []; // array will hold the errors
+
+    if (!validator.isEmail(userInput.email)) {
+      errors.push({ message: "Email is invalid!" });
+    }
+    if (!validator.isLength(userInput.password, { min: 5 })) {
+      errors.push({ message: "Password must be at least 5 characters!" });
+    }
+    if (errors.length > 0) {
+      const error = new Error("Input validation failed!");
+      error.data = errors;
+      error.code = 422;
+      throw error;
+    }
+    return User.findOne({ email: userInput.email })
       .then(user => {
         if (user) {
-          const error = new Error("User not found in database!");
-          next(error);
+          const error = new Error("Can't find the user!");
+          throw error;
         }
         return bcrypt.hash(userInput.password, 12);
       })
       .then(encryptedPassword => {
         const user = new User({
-          email: email,
+          email: userInput.email,
           name: userInput.name,
           password: encryptedPassword,
         });
